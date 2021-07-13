4f2c489c18a806b236f1fcb406903712051cd9d1
srini <troubleshootingandtweaks@gmail.com>
2021-07-13 02:11:16 +0000

    commiting from child

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ class Hello
 {
 public static void main(String args[])
 {
-System.out.println("third commit ,changing author information");
+System.out.println(" commit from child ,changing author information");
 }
 }
