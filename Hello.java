2c6c0a8bd54ceafb5e8bc098febe55e26dcd8362
srikanthprathivadi <troubleshootingandtweaks@gmail.com>
2021-07-13 08:46:15 +0530

    commit from remote branch 2nd time,
     commit from remote branch 2nd time,

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ class Hello
 {
 public static void main(String args[])
 {
-System.out.println(" commit from remote branch ,changing author information");
+System.out.println(" commit from remote branch 2nd time,changing author information");
 }
 }
